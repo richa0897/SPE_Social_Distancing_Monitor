309c8e1450c36cf08390552de8aaa98a8b74a6fe
Richa Varma <varma.richavarma@gmail.com>
2022-05-08 17:50:55 +0530

    changed app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 2048 * 2048
 
 # Enter your database connection details below
-app.config['MYSQL_HOST'] = 'mysqldb'
+app.config['MYSQL_HOST'] = '0.0.0.0'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'spe_proj'
